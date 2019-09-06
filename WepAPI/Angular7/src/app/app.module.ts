8e62c5a600f53797c6898c88846d965474622021
ozhangulal <ozhangulal@outlook.com>
2019-09-06 17:50:30 +0300

    DotNetCoreWebAPIwithAngular7UserRegistration
    using dot net core web api and angular 7 user registration app

---
+++ WepAPI/Angular7/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserService } from './shared/user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WepAPI/Angular7/src/app/user/registration/registration.component.ts
@@ -0,0 +1,46 @@
+import {UserService} from './../../shared/user.service';
+import { Component, OnInit } from '@angular/core';
+// import { UserService } from 'src/app/shared/user.service';
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(public service:UserService) { }
+
+  ngOnInit() {
+    this.service.formModel.reset();
+  }
+
+  onSubmit()
+  {
+
+    this.service.register().subscribe(
+      (res:any)=>{
+        if(res.succeeded){
+          this.service.formModel.reset();
+        }else{
+          res.errors.forEach(element => {
+            switch(element.code){
+              case 'DuplicateUserName':
+                //Username is already taken
+                break;
+
+                default:
+                  //Registration failed
+                  break;
+            }
+          });
+          
+        }
+      },
+      err=>{console.log(err);}
+    )
+
+  }
+
+}
